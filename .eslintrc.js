1aa938e95b03a4c9a95603b74b5e8764de581fe4
AnisLucidi01 <125553099+AnisLucidi01@users.noreply.github.com>
2024-04-05 11:27:10 +0200

    chore: [P4PADEV-130] Added and configured eslint, prettier, typescript, doten… (#1)

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: 'tsconfig.json',
+      tsconfigRootDir: __dirname,
+      sourceType: "module"
+    },
+    plugins: ['@typescript-eslint',],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+      'plugin:prettier/recommended',
+    ],
+    "ignorePatterns": ["**/__integration_tests__/*"],
+
+  };
