7ce04363b6101f288a1929e142e4b31506e48671
anislucidi01 <anis.lucidi@pagopa.it>
2024-04-11 13:33:01 +0200

    P4ADEV-166: Created story for footer.  Adapted lang file.

--- src/translations/lang.ts
+++ src/translations/lang.ts
@@ -1,37 +1,16 @@
 import translationIT from './it/translations.json';
+import { Languages } from '@pagopa/mui-italia';
 
-const lang: Languages = {
-  it: {
-    it: 'Italiano',
-    en: 'Inglese',
-    fr: 'Francese',
-    label: 'Italiano',
-    lang: 'it-IT',
-    translation: translationIT
-  }
-};
-
-export interface Languages {
-  [key: string]: {
-    it: string;
-    en: string;
-    fr: string;
-    label: string;
-    lang: string;
-    translation: typeof translationIT;
+const lang: Languages & { it: { label: string; lang: string; translation: typeof translationIT } } =
+  {
+    it: {
+      it: 'Italiano',
+      en: 'Inglese',
+      fr: 'Francese',
+      label: 'Italiano',
+      lang: 'it-IT',
+      translation: translationIT
+    }
   };
-}
-
-export function getSortedLang(): Array<{
-  label: string;
-  lang: string;
-}> {
-  return Object.keys(lang)
-    .sort()
-    .reduce((obj: Array<{ label: string; lang: string }>, key: string) => {
-      obj.push(lang[key]);
-      return obj;
-    }, []);
-}
-
+/* Are we sure we need a sorting function? The order wouldn't be the one commonly intended by us (which is usually: IT, EN, FR...)*/
 export default lang;
