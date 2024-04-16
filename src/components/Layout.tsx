b6975a026f7ecad5b7b131391f7186a4dac9b4d7
Vito Falco <vitofalco@gmail.com>
2024-04-16 11:06:16 +0200

    Add sidebar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,6 @@ import { SxProps } from '@mui/system';
 import { Footer } from './Footer';
 import { Header } from './Header';
 import { Sidebar } from './Sidebar';
-import style from 'src/utils/style';
 import { grey } from '@mui/material/colors';
 
 interface LayoutProps {
@@ -19,10 +18,8 @@ export function Layout({ children }: LayoutProps) {
         <Grid item xs={12}>
           <Header />
         </Grid>
-        <Grid item xs={3} height={`calc(100vh - (${style.header.height + style.footer.height}px))`}>
-          <Sidebar />
-        </Grid>
-        <Grid item xs={9} bgcolor={grey['100']} padding={4}>
+        <Sidebar />
+        <Grid item bgcolor={grey['100']} padding={4}>
           {children}
         </Grid>
         <Grid item xs={12}>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,63 @@
-import { Box } from '@mui/material';
-import React from 'react';
+import { Box, Divider, Grid, IconButton, List } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import { SidebarMenuItem } from './SidebarMenuItem';
+import { ISidebarMenuItem } from 'src/models/SidebarMenuItem';
+import MenuIcon from '@mui/icons-material/Menu';
+import React, { useState } from 'react';
+import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
+import Tooltip from '@mui/material/Tooltip';
+import ViewSidebarIcon from '@mui/icons-material/ViewSidebar';
 
-export const Sidebar = () => <Box>Sidebar</Box>;
+export const Sidebar = () => {
+  const { t } = useTranslation();
+
+  const [collapsed, setCollapsed] = useState<boolean>(false);
+
+  const menuItems: Array<ISidebarMenuItem> = [
+    {
+      label: t('menu.homepage'),
+      icon: ViewSidebarIcon,
+      route: '/'
+    },
+    {
+      label: t('menu.receipts'),
+      icon: ReceiptLongIcon,
+      route: '/'
+    }
+  ];
+  return (
+    <>
+      <Grid
+        alignItems={'normal'}
+        display={'flex'}
+        flexDirection={'column'}
+        item
+        minHeight={'50vh'}
+        xs={collapsed ? 'auto' : 3}>
+        <List role="navigation" component="nav" aria-label={t('menu.description')}>
+          {menuItems.map((item: ISidebarMenuItem, index: number) => (
+            <SidebarMenuItem collapsed={collapsed} item={item} key={index}></SidebarMenuItem>
+          ))}
+        </List>
+        <Divider orientation="horizontal" flexItem />
+        <Box marginTop={'auto'} aria-hidden="true">
+          <Divider orientation="horizontal" flexItem />
+          <Box p={2}>
+            <Tooltip
+              placement="right"
+              title={t(!collapsed ? 'sidebar.collapse' : 'sidebar.expand')}>
+              <IconButton
+                aria-label={t(!collapsed ? 'sidebar.collapse' : 'sidebar.expand')}
+                onClick={() => {
+                  setCollapsed(!collapsed);
+                }}
+                size="large">
+                <MenuIcon />
+              </IconButton>
+            </Tooltip>
+          </Box>
+        </Box>
+      </Grid>
+    </>
+  );
+};
