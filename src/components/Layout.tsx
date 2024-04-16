8c6fdf1a6408770ae72f56b11c0a878aab9d4cc2
anislucidi01 <anis.lucidi@pagopa.it>
2024-04-16 20:41:11 +0200

    P4ADEV-166: Minor fixes.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,9 +4,8 @@ import React from 'react';
 import { Header } from './Header';
 import { Sidebar } from './Sidebar';
 import style from 'src/utils/style';
-import { Footer, LangCode } from '@pagopa/mui-italia';
-import { useTranslation } from 'react-i18next';
-import lang from '../translations/lang';
+import { LangCode } from '@pagopa/mui-italia';
+import { Footer } from './Footer';
 
 interface LayoutProps {
   sx?: SxProps;
@@ -16,12 +15,10 @@ interface LayoutProps {
 }
 
 export function Layout({ children, currentLanguage, changeLanguage }: LayoutProps) {
-  const { t } = useTranslation();
-
   const theme = useTheme();
   return (
     <Container
-      maxWidth={false}
+      maxWidth={'lg'}
       disableGutters
       sx={{
         bgcolor: theme.palette.background.default
@@ -37,73 +34,7 @@ export function Layout({ children, currentLanguage, changeLanguage }: LayoutProp
           {children}
         </Grid>
         <Grid item xs={12}>
-          <Footer
-            loggedUser
-            companyLink={{ ariaLabel: 'PagoPA SPA' }}
-            legalInfo={
-              <>
-                <b>{t('general.PagoPA')}</b> - {t('ui.footer.legalInfo')}
-              </>
-            }
-            postLoginLinks={[
-              {
-                label: t('ui.footer.privacy'),
-                ariaLabel: t('ui.footer.privacy'),
-                linkType: 'internal'
-              },
-              {
-                label: t('ui.footer.personalData'),
-                ariaLabel: t('ui.footer.personalData'),
-                linkType: 'internal'
-              },
-              {
-                label: t('ui.footer.termsAndConditions'),
-                ariaLabel: t('ui.footer.termsAndConditions'),
-                linkType: 'internal'
-              },
-              { label: t('aria.a11y'), ariaLabel: t('aria.a11y'), linkType: 'internal' }
-            ]}
-            preLoginLinks={{
-              aboutUs: {
-                links: [
-                  {
-                    label: 'string',
-                    /** the url to witch the user will be redirect */
-                    ariaLabel: 'string',
-                    linkType: 'internal'
-                  }
-                ]
-              },
-              resources: {
-                links: [
-                  {
-                    label: 'string',
-                    /** the url to witch the user will be redirect */
-                    ariaLabel: 'string',
-                    linkType: 'internal'
-                  }
-                ]
-              },
-              followUs: {
-                title: 'string',
-                socialLinks: [
-                  {
-                    icon: 'string',
-                    /** the url to witch the user will be redirect */
-                    title: 'string',
-                    ariaLabel: 'string'
-                  }
-                ],
-                links: [{ label: 'string', ariaLabel: 'string', linkType: 'internal' }]
-              }
-            }}
-            currentLangCode={currentLanguage}
-            languages={lang}
-            onExit={() => {}}
-            productsJsonUrl="https://dev.selfcare.pagopa.it/assets/products.json"
-            hideProductsColumn={false}
-            onLanguageChanged={changeLanguage}
-          />
+          <Footer currentLanguage={currentLanguage} changeLanguage={changeLanguage} />
         </Grid>
       </Grid>
     </Container>
