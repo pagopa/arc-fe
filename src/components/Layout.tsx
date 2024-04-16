acb531de09215ef1e0ed862a76a5fe4adaa3b27c
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2024-04-16 11:24:07 +0200

    [P4ADEV-169]: using the right background color

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,11 @@
-import { Container, Grid, useTheme } from '@mui/material';
-import { SxProps } from '@mui/system';
 import React from 'react';
+import { Container, Grid } from '@mui/material';
+import { SxProps } from '@mui/system';
 import { Footer } from './Footer';
 import { Header } from './Header';
 import { Sidebar } from './Sidebar';
 import style from 'src/utils/style';
+import { grey } from '@mui/material/colors';
 
 interface LayoutProps {
   sx?: SxProps;
@@ -12,13 +13,8 @@ interface LayoutProps {
 }
 
 export function Layout({ children }: LayoutProps) {
-  const theme = useTheme();
   return (
-    <Container
-      maxWidth="lg"
-      sx={{
-        bgcolor: theme.palette.background.default
-      }}>
+    <Container maxWidth="lg">
       <Grid container>
         <Grid item xs={12}>
           <Header />
@@ -26,7 +22,7 @@ export function Layout({ children }: LayoutProps) {
         <Grid item xs={3} height={`calc(100vh - (${style.header.height + style.footer.height}px))`}>
           <Sidebar />
         </Grid>
-        <Grid item xs={9}>
+        <Grid item xs={9} bgcolor={grey['100']} padding={4}>
           {children}
         </Grid>
         <Grid item xs={12}>
