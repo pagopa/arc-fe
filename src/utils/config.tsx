4482efe0d18291d5aff3e91d9aedc8b652eb49b5
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2024-04-17 11:41:20 +0200

    [P4ADEV-169] header component creation

---
+++ src/utils/config.tsx
@@ -0,0 +1,51 @@
+import { ProductEntity, RootLinkType, UserAction } from '@pagopa/mui-italia';
+/* Icons */
+import SettingsIcon from '@mui/icons-material/Settings';
+import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+
+type Config = {
+  product: ProductEntity;
+  pagopaLink: RootLinkType;
+  userActions: UserAction[];
+};
+
+const product: ProductEntity = {
+  id: '0',
+  title: `Area Riservata Cittadini`,
+  productUrl: '#area-riservata-cittadini',
+  linkType: 'external'
+};
+
+const pagopaLink: RootLinkType = {
+  label: 'PagoPA S.p.A.',
+  href: 'https://www.pagopa.it/',
+  ariaLabel: 'Link: vai al sito di PagoPA S.p.A.',
+  title: 'Sito di PagoPA S.p.A.'
+};
+
+const userActions: UserAction[] = [
+  {
+    id: 'profile',
+    label: 'I tuoi dati',
+    onClick: () => {
+      console.log('Clicked/Tapped on Profile');
+    },
+    icon: <SettingsIcon fontSize="small" color="inherit" />
+  },
+  {
+    id: 'logout',
+    label: 'Esci',
+    onClick: () => {
+      console.log('User logged out');
+    },
+    icon: <LogoutRoundedIcon fontSize="small" color="inherit" />
+  }
+];
+
+const config: Config = {
+  product,
+  pagopaLink,
+  userActions
+};
+
+export default config;
