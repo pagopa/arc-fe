57d4f1afc8991306295553b3f334d355b605607c
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2024-04-23 11:18:45 +0200

    [P4ADEV-204] Info alert added to dashboard route

---
+++ src/components/Alerts/IOAlert.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Alert, AlertTitle, Button, Typography } from '@mui/material';
+import { theme } from '@pagopa/mui-italia';
+import { useTranslation } from 'react-i18next';
+
+const IOAlert = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Alert
+      severity="info"
+      sx={{
+        borderLeft: `solid 4px ${theme.palette.info.main}`,
+        '& .MuiAlert-icon': {
+          color: 'info.contrastText',
+          alignItems: 'center'
+        }
+      }}>
+      <AlertTitle sx={{ marginBottom: 0 }}>{t('app.dashboard.IOAlert.title')}</AlertTitle>
+      <Typography variant="body2" mb={1}>
+        {t('app.dashboard.IOAlert.description')}
+      </Typography>
+      <Button>{t('app.dashboard.IOAlert.cta')}</Button>
+    </Alert>
+  );
+};
+
+export default IOAlert;
