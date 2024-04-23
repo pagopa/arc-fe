ced0349d773634147a06e7c68c0209a4b1fa4d9a
Strato Ivan Di Luccio <34749257+stratoivandiluccio@users.noreply.github.com>
2024-04-23 15:49:07 +0200

    feat: [P4ADEV-204] Info alert added to dashboard route (#13)

--- src/utils/config.test.ts
+++ src/utils/config.test.ts
@@ -1,14 +1,14 @@
 import { renderHook, act } from '@testing-library/react';
-import hooks from './hooks';
+import utils from '.';
 
 describe('useLanguage hook', () => {
   it('should return the inital default it language correctly', () => {
-    const { result } = renderHook(() => hooks.useLanguage());
+    const { result } = renderHook(() => utils.hooks.useLanguage());
     expect(result.current.language).toBe('it');
   });
 
   it('should change the language to en correctly passing the langCode as argument to changeLanguage', () => {
-    const { result } = renderHook(() => hooks.useLanguage());
+    const { result } = renderHook(() => utils.hooks.useLanguage());
     act(() => {
       result.current.changeLanguage('en');
     });
