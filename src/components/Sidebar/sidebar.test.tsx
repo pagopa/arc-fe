0bbbe7db44f39fc3e56d43b05bea828b00f41044
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2024-04-24 13:08:27 +0200

    [P4ADEV] lint

--- src/components/Sidebar/sidebar.test.tsx
+++ src/components/Sidebar/sidebar.test.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import { renderHook, act, render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import useCollapseMenu from './useCollapseMenu';
-import Sidebar  from './';
-import { BrowserRouter } from "react-router-dom";
+import Sidebar from './';
+import { BrowserRouter } from 'react-router-dom';
 import '../../translations/i18n';
 
 describe('useCollapseMenu hook', () => {
@@ -24,19 +24,19 @@ describe('useCollapseMenu hook', () => {
 describe('Sidebar component', () => {
   it('should render as expecteed', () => {
     render(
-    <BrowserRouter>
-      <Sidebar />
-    </BrowserRouter>
+      <BrowserRouter>
+        <Sidebar />
+      </BrowserRouter>
     );
   });
 
   it('should collapse correctly', () => {
     const result = render(
-    <BrowserRouter>
-      <Sidebar />
-    </BrowserRouter>
+      <BrowserRouter>
+        <Sidebar />
+      </BrowserRouter>
     );
-    
+
     expect(result.container.querySelector('#menu-item-homepage')).toBeInTheDocument();
     fireEvent.click(screen.getByLabelText('Comprimi il menu'));
     expect(result.container.querySelector('#menu-item-homepage')).not.toBeInTheDocument();
