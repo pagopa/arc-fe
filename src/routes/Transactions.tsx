9ef1dc3716af2b59f30cdce6ce49c78bd9a8a7b8
anislucidi01 <anis.lucidi@pagopa.it>
2024-04-24 10:25:19 +0200

    P4ADEV-210: Implemented order by filter.

--- src/routes/Transactions.tsx
+++ src/routes/Transactions.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import Tabs from 'components/Tabs';
 import Transactions from 'components/Transactions/Transactions';
-import { Button, InputAdornment, Stack, TextField, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  FormControl,
+  Grid,
+  InputAdornment,
+  InputLabel,
+  MenuItem,
+  Select,
+  Stack,
+  TextField,
+  Typography
+} from '@mui/material';
 import { useTranslation } from 'react-i18next';
 
 // TO BE REMOVED
@@ -15,35 +27,62 @@ export default function Dashboard() {
     <>
       <Stack direction="row" justifyContent="space-between" alignItems="center" mb={5}>
         <Typography variant="h3">{t('menu.receipts')}</Typography>
-        <Button
-          variant="contained"
-          size="large"
-          href="https://uat.checkout.pagopa.it/"
-          target="_blank">
-          {t('app.dashboard.newPayment')}
-        </Button>
       </Stack>
-      <Stack direction="row" justifyContent="space-between" alignItems="center" mb={2}>
-        <TextField
-          size="small"
-          fullWidth={true}
-          label={t('app.transactions.searchByName')}
-          InputLabelProps={{
-            sx: {
-              color: theme.palette.text.secondary,
-              fontWeight: 600,
-            }
-          }}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="start">
-                <Search />
-              </InputAdornment>
-            )
-          }}
-        />
-        
-      </Stack>
+      <Grid container spacing={3} mb={3}>
+        <Grid item xs={9}>
+          <TextField
+            size="small"
+            fullWidth={true}
+            label={t('app.transactions.searchByName')}
+            InputLabelProps={{
+              sx: {
+                color: theme.palette.text.secondary,
+                fontWeight: 600
+              }
+            }}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="start">
+                  <Search />
+                </InputAdornment>
+              )
+            }}
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <Box>
+            <FormControl fullWidth>
+              <InputLabel
+                size="small"
+                sx={{ fontWeight: 600, fontSize: theme.typography.body2.fontSize }}>
+                {t('app.transactions.orderBy')}:
+              </InputLabel>
+
+              <Select
+                id="search-by"
+                label={t('app.transactions.searchBy')}
+                labelId="search-by-label"
+                size="small"
+                fullWidth
+                onChange={function noRefCheck() {}}
+                value="">
+                <MenuItem selected value="it-health-code">
+                  {t('app.transactions.mostRecent')}
+                </MenuItem>
+                <MenuItem selected value="it-health-code">
+                  {t('app.transactions.leastRecent')}
+                </MenuItem>
+                <MenuItem selected value="it-health-code">
+                  {t('app.transactions.lowerAmount')}
+                </MenuItem>
+                <MenuItem selected value="it-health-code">
+                  {t('app.transactions.higherAmount')}
+                </MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+        </Grid>
+      </Grid>
       <Tabs
         initialActiveTab={0}
         tabs={[
