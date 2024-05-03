093e969f8e4d459b031f48f8e8f19f75a1544ae0
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2024-05-03 16:25:22 +0200

    [P4ADEV-271] another testing strategy

--- jest.config.ts
+++ jest.config.ts
@@ -104,7 +104,6 @@ const config: Config = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
-    'generated/(.*)': '<rootDir>/__mocks__/$1',
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
