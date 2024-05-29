82ef997a52b1e93db8cb2d4f5e081adc0a952e1e
anislucidi01 <anis.lucidi@pagopa.it>
2024-05-29 09:53:53 +0200

    P4PU-98: Merge fixes.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -33,11 +33,21 @@ export function Layout() {
         <Grid flexBasis={{ xs: 'fit-content' }} item xs={12} height="fit-content">
           <Header />
         </Grid>
-        {sidebar?.visible ? <Sidebar /> : null}
-        <Grid item bgcolor={grey['100']} padding={3} xs>
-          {backButton && <BackButton />}
-          {crumbs && <Breadcrumbs crumbs={crumbs} separator={<NavigateNext fontSize="small" />} />}
-          <Outlet />
+        <Grid
+          item
+          display={'flex'}
+          flexGrow={1}
+          flexWrap={'wrap'}
+          alignContent={'flex-start'}
+          flexBasis={'50vh'}>
+          {sidebar?.visible ? <Sidebar /> : null}
+          <Grid item bgcolor={grey['100']} padding={3} height={'100%'} xs>
+            {backButton && <BackButton />}
+            {crumbs && (
+              <Breadcrumbs crumbs={crumbs} separator={<NavigateNext fontSize="small" />} />
+            )}{' '}
+            <Outlet />
+          </Grid>
         </Grid>
         <Grid item xs={12} height="fit-content" flexBasis={{ xs: 'fit-content' }} flexShrink={3}>
           {/*xs in flex basis is specified to override mui clas.*/}
