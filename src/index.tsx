8445afb8ac7c141fa275187c0d5c47078b1c3ddd
anislucidi01 <anis.lucidi@pagopa.it>
2024-05-30 10:54:14 +0200

    P4PU-92: Added interceptor

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,19 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import utils from 'utils';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 const queryClient = new QueryClient();
+
+utils.apiClient.instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.log(error.response.status)
+    throw error;
+  }
+);
 
 root.render(
   <React.StrictMode>
