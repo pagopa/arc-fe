800865b4b62188ee82b970b75c8dd12ee4d284a7
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2024-06-18 10:16:50 +0200

    [P4PU-195] jest default env variables updated

--- .jest/setEnvVars.js
+++ .jest/setEnvVars.js
@@ -1,4 +1,5 @@
 process.env.APIHOST = "http://localhost:1234/api";
 process.env.ENV="LOCAL";
 process.env.ENTITIES_LOGO_CDN="https://assets.cdn.io.italia.it/logos/organizations";
-process.env.SHOW_STATUS_INFO='false';
+process.env.ASSISTANCE_LINK="https://pagopa.gov.it"
+process.env.SHOW_STATUS_INFO='true';
