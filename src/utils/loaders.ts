08738662b6cf9f9af03031e219d77e6dea36c5d2
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2024-06-20 13:29:09 +0200

    [P4PU-187] swagger pointer updated

--- src/utils/loaders.ts
+++ src/utils/loaders.ts
@@ -5,21 +5,24 @@ const getTransactions = () =>
   useQuery({
     queryKey: ['transactions'],
     queryFn: async () => {
-      const { data: transactions } = await utils.apiClient.transactions.getTransactionList();
-      utils.zodSchema.transactionsResponseSchema.parse(transactions);
+      const { data: transactions } = await utils.apiClient.transactions.getTransactionsList();
+      utils.zodSchema.transactionsListDTOSchema.parse(transactions);
       return transactions;
     }
   });
 
-const getTransactionDetails = (id: string) =>
-  useQuery({
-    queryKey: ['transactionDetail'],
-    queryFn: async () => {
-      const { data: transaction } = await utils.apiClient.transactions.getTransactionDetails(id);
-      utils.zodSchema.transactionDetailResponseSchema.parse(transaction);
-      return transaction;
-    }
-  });
+const getTransactionDetails = (id: string) => {
+  console.log(id);
+  return {};
+};
+// useQuery({
+//   queryKey: ['transactionDetail'],
+//   queryFn: async () => {
+//     const { data: transaction } = await utils.apiClient.transactions.getTransactionDetails(id);
+//     utils.zodSchema.transactionDetailResponseSchema.parse(transaction);
+//     return transaction;
+//   }
+// });
 
 export default {
   getTransactions,
