b7eb842db47dae46a28b1002ad673de56dac837c
anislucidi01 <anis.lucidi@pagopa.it>
2024-06-25 12:04:40 +0200

    P4PU-213: Added mock function for error on payment notices list.

--- src/routes/PaymentNotices.tsx
+++ src/routes/PaymentNotices.tsx
@@ -4,8 +4,10 @@ import Typography from '@mui/material/Typography';
 import { ButtonNaked } from '@pagopa/mui-italia';
 import { PaymentNotice } from 'components/PaymentNotice';
 import { CardProps } from 'components/PaymentNotice/Card';
+import QueryLoader from 'components/QueryLoader';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import utils from 'utils';
 
 const paymentNoticesMock: Array<CardProps & { id: number }> = [
   {
@@ -50,69 +52,81 @@ const updatedDate = new Date();
 
 export const PaymentNotices = () => {
   const { t } = useTranslation();
+  const { data, isError } = utils.loaders.getPaymentNotices();
 
   return (
     <Stack height="100%" justifyContent={{ lg: 'space-between' }} component="main">
-      <Stack gap={5} component="section">
-        <Typography paddingTop={3} variant="h4" component="h1">
-          {t('app.paymentNotices.title')}
-        </Typography>
-        <Stack gap={3}>
-          <Stack
-            direction="row"
-            justifyContent="space-between"
-            alignItems="center"
-            component="header">
-            <Typography variant="h6" component="h2">
-              {`${t('app.paymentNotices.found')} ${paymentNoticesMock.length} ${t('app.paymentNotices.notice')}`}
-            </Typography>
-            <Typography
-              display="flex"
-              variant="body1"
-              component="div"
-              flexDirection="row"
-              gap="5px">
-              <Typography color="text.secondary" component="span">
-                {t('app.paymentNotices.updated')}
-              </Typography>
-              <Typography
-                sx={{ fontWeight: 600 }}
-                component="time"
-                dateTime={updatedDate.toISOString()}>
-                {updatedDate.toLocaleString(navigator.language, {
-                  timeStyle: 'short',
-                  dateStyle: 'medium'
-                })}
-              </Typography>
-            </Typography>
-          </Stack>
-          <Stack gap={3} component="section">
-            {paymentNoticesMock.map((paymentNotice) => (
-              <PaymentNotice.Card key={paymentNotice.id} {...paymentNotice} />
-            ))}
-          </Stack>
+      <QueryLoader
+        // TODO: fallback component of behavior be defined
+        fallback={isError && <PaymentNotice.Error />}
+        queryKey="paymentNotices">
+        <Stack gap={5} component="section">
+          <Typography paddingTop={3} variant="h4" component="h1">
+            {t('app.paymentNotices.title')}
+          </Typography>
+          {paymentNoticesMock.length > 0 ? (
+            <Stack gap={3}>
+              <Stack
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
+                component="header">
+                <Typography variant="h6" component="h2">
+                  {`${t('app.paymentNotices.found')} ${paymentNoticesMock.length} ${t('app.paymentNotices.notice')}`}
+                </Typography>
+                <Typography
+                  display="flex"
+                  variant="body1"
+                  component="div"
+                  flexDirection="row"
+                  gap="5px">
+                  <Typography color="text.secondary" component="span">
+                    {t('app.paymentNotices.updated')}
+                  </Typography>
+                  <Typography
+                    sx={{ fontWeight: 600 }}
+                    component="time"
+                    dateTime={updatedDate.toISOString()}>
+                    {updatedDate.toLocaleString(navigator.language, {
+                      timeStyle: 'short',
+                      dateStyle: 'medium'
+                    })}
+                  </Typography>
+                </Typography>
+              </Stack>
+              <Stack gap={3} component="section">
+                {paymentNoticesMock.map((paymentNotice) => (
+                  <PaymentNotice.Card key={paymentNotice.id} {...paymentNotice} />
+                ))}
+              </Stack>
+            </Stack>
+          ) : (
+            <PaymentNotice.Empty />
+          )}
         </Stack>
-      </Stack>
-      <Stack
-        padding={1.3}
-        gap={1}
-        justifyContent="flex-start"
-        marginTop={{ xs: 10, lg: 0 }}
-        component="aside">
-        <Typography variant="body1" component="p">
-          {t('app.paymentNotices.alert.info')}
-        </Typography>
-        <Typography color="error" component="div">
-          <ButtonNaked
-            variant="text"
-            size="medium"
-            color="inherit"
-            startIcon={<ErrorOutline />}
-            aria-label={t('app.paymentNotices.alert.action')}>
-            {t('app.paymentNotices.alert.action')}
-          </ButtonNaked>
-        </Typography>
-      </Stack>
+        {data?.length && data?.length > 0 && (
+          <Stack
+            padding={1.3}
+            gap={1}
+            justifyContent="flex-start"
+            marginTop={{ xs: 10, lg: 0 }}
+            component="aside">
+            <Typography variant="body1" component="p">
+              {t('app.paymentNotices.alert.info')}
+            </Typography>
+            <Typography color="error" component="div">
+              <ButtonNaked
+                variant="text"
+                size="medium"
+                color="inherit"
+                startIcon={<ErrorOutline />}
+                aria-label={t('app.paymentNotices.alert.action')}>
+                {t('app.paymentNotices.alert.action')}
+              </ButtonNaked>
+            </Typography>
+          </Stack>
+        )}
+      </QueryLoader>
     </Stack>
   );
 };
