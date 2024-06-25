6479099de19e4aedbb16a20bb936542314805bf9
anislucidi01 <anis.lucidi@pagopa.it>
2024-06-25 12:57:30 +0200

    p4pu-181 Added test

---
+++ src/components/PaymentNotice/Detail.test.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { useTranslation } from 'react-i18next';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { PaymentNotice } from './index';
+
+jest.mock('react-i18next', () => ({
+  useTranslation: jest.fn()
+}));
+
+describe('Detail Component', () => {
+  const renderWithTheme = (ui: React.ReactElement) => {
+    const theme = createTheme();
+    return render(<ThemeProvider theme={theme}>{ui}</ThemeProvider>);
+  };
+
+  beforeEach(() => {
+    (useTranslation as jest.Mock).mockReturnValue({
+      t: (key: string) => key
+    });
+  });
+
+  const paymentNoticeDetail = {
+    amount: '10',
+    paFullName: 'paFullName',
+    subject: 'subject',
+    dueDate: 'dueDate',
+    iupd: 'iupd',
+    paTaxCode: 'paTaxCode',
+    firstInstallmentDate: 'firstInstallmentDate',
+    firstInstallmentAmount: '0'
+  };
+  it('renders the notice data', () => {
+    renderWithTheme(<PaymentNotice.Detail paymentNoticeDetail={paymentNoticeDetail} />);
+
+    expect(screen.getByText('10')).toBeInTheDocument();
+    expect(screen.getByText('paFullName')).toBeInTheDocument();
+    expect(screen.getByText('subject')).toBeInTheDocument();
+    expect(screen.getByText('dueDate')).toBeInTheDocument();
+    expect(screen.getByText('paTaxCode')).toBeInTheDocument();
+    expect(screen.getByText('iupd')).toBeInTheDocument();
+    expect(screen.getByText('firstInstallmentDate')).toBeInTheDocument();
+    expect(screen.getByText('0')).toBeInTheDocument();
+  });
+});
