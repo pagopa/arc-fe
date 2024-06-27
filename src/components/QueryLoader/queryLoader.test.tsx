44c0a4ee4f2c1bfbb413e25119fc77c3bdfdd1a6
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2024-06-27 11:42:41 +0200

    [P4PU-196] unit test extended

--- src/components/QueryLoader/queryLoader.test.tsx
+++ src/components/QueryLoader/queryLoader.test.tsx
@@ -3,10 +3,15 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import QueryLoader from '.';
 import { QueryFilters } from '@tanstack/react-query';
+import { CircularProgress } from '@mui/material';
 
 const mockeduseIsFetching = jest.fn();
 jest.mock('@tanstack/react-query', () => ({
   useIsFetching: (filters?: QueryFilters) => mockeduseIsFetching(filters)
+}));
+
+jest.mock('@mui/material', () => ({
+  CircularProgress: jest.fn()
 }));
 
 describe('Query Loader component', () => {
@@ -19,6 +24,7 @@ describe('Query Loader component', () => {
     );
     expect(mockeduseIsFetching).toHaveBeenCalledTimes(1);
     expect(mockeduseIsFetching).toHaveBeenCalledWith({ queryKey: ['testQueryKey'] });
+    expect(CircularProgress).not.toHaveBeenCalled();
     expect(screen.getByText('test loader')).toBeInTheDocument();
     expect(screen.queryByText('test children')).toBeNull();
   });
@@ -45,5 +51,16 @@ describe('Query Loader component', () => {
     expect(mockeduseIsFetching).toHaveBeenCalledTimes(0);
     expect(screen.queryByText('test children')).toBeNull();
     expect(screen.getByText('test fallback')).toBeInTheDocument();
+  });
+
+  it('should render the default loader component', () => {
+    mockeduseIsFetching.mockReturnValueOnce(true);
+    render(
+      <QueryLoader queryKey="testQueryKey">
+        <p>test children</p>
+      </QueryLoader>
+    );
+    expect(mockeduseIsFetching).toHaveBeenCalledTimes(1);
+    expect(CircularProgress).toHaveBeenCalled();
   });
 });
