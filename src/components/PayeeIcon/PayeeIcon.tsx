bbd79180837aa9fb4e6da78b2496bb571460e19f
Vito Falco <vitofalco@gmail.com>
2024-06-27 09:25:42 +0200

    feat: [P4PU-227]  fallback image (#58)

--- src/components/PayeeIcon/PayeeIcon.tsx
+++ src/components/PayeeIcon/PayeeIcon.tsx
@@ -1,5 +1,5 @@
 import Box from '@mui/material/Box';
-import React from 'react';
+import React, { SyntheticEvent } from 'react';
 import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 import style from 'utils/style';
 
@@ -9,24 +9,35 @@ export interface payeeIconProps {
   visible?: boolean;
 }
 
-export const PayeeIcon = (props: payeeIconProps) => (
-  <Box
-    width={48}
-    height={48}
-    border={`solid 1px ${style.theme.palette.divider}`}
-    borderRadius={6}
-    alignItems="center"
-    display={props.visible ? 'flex' : 'none'}
-    justifyContent="center">
-    {props.src ? (
-      <img
-        src={props.src}
-        alt={props?.alt ? props.alt : 'Logo Ente'}
-        aria-hidden="true"
-        style={{ width: '55%' }}
-      />
-    ) : (
-      <AccountBalanceIcon sx={{ color: style.theme.palette.grey[400] }} />
-    )}
-  </Box>
-);
+export const PayeeIcon = (props: payeeIconProps) => {
+  function onErrorImage(e: SyntheticEvent) {
+    (e.target as HTMLImageElement).src = '/images/fallback-ec.png';
+    return;
+  }
+
+  return (
+    <Box
+      width={48}
+      height={48}
+      border={`solid 1px ${style.theme.palette.divider}`}
+      borderRadius={6}
+      alignItems="center"
+      display={props.visible ? 'flex' : 'none'}
+      justifyContent="center">
+      {props.src ? (
+        <img
+          src={props.src}
+          alt={props?.alt ? props.alt : 'Logo Ente'}
+          aria-hidden="true"
+          data-testid="payeelogoimg"
+          style={{ width: '55%' }}
+          onError={(e) => {
+            onErrorImage(e);
+          }}
+        />
+      ) : (
+        <AccountBalanceIcon sx={{ color: style.theme.palette.grey[400] }} />
+      )}
+    </Box>
+  );
+};
