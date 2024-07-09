2e232955cd00a36dee7b931048efa81cbdc36040
anislucidi01 <anis.lucidi@pagopa.it>
2024-07-09 14:02:08 +0200

    P4PU-270: Test fixes.

--- src/components/PaymentNotice/Card.test.tsx
+++ src/components/PaymentNotice/Card.test.tsx
@@ -5,13 +5,20 @@ import { useTranslation } from 'react-i18next';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { CardProps } from './Card';
 import { PaymentNotice } from './index';
+import { BrowserRouter } from 'react-router-dom';
 
 jest.mock('react-i18next', () => ({
   useTranslation: jest.fn()
 }));
+const mockedUsedNavigate = jest.fn();
 
 jest.mock('components/PayeeIcon', () => ({
   PayeeIcon: jest.fn(() => <div>Payee Icon</div>)
+}));
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockedUsedNavigate
 }));
 
 describe('Card Component', () => {
@@ -27,6 +34,7 @@ describe('Card Component', () => {
   });
 
   const baseProps: CardProps = {
+    id: '123',
     payee: {
       name: 'Payee Name',
       srcImg: 'payee.png',
@@ -38,28 +46,41 @@ describe('Card Component', () => {
   };
 
   it('renders the payee name, payment info, and amount', () => {
-    renderWithTheme(<PaymentNotice.Card {...baseProps} />);
-
+    renderWithTheme(
+      <BrowserRouter>
+        <PaymentNotice.Card {...baseProps} />
+      </BrowserRouter>
+    );
     expect(screen.getByText('Payee Name')).toBeInTheDocument();
     expect(screen.getByText('Payment Info')).toBeInTheDocument();
     expect(screen.getByText('100')).toBeInTheDocument();
   });
 
   it('renders the expiring date if provided', () => {
-    renderWithTheme(<PaymentNotice.Card {...baseProps} />);
-
+    renderWithTheme(
+      <BrowserRouter>
+        <PaymentNotice.Card {...baseProps} />
+      </BrowserRouter>
+    );
     expect(screen.getByText('app.paymentNotice.card.expiring')).toBeInTheDocument();
     expect(screen.getByText('2022-12-31')).toBeInTheDocument();
   });
 
   it('renders the PayeeIcon', () => {
-    renderWithTheme(<PaymentNotice.Card {...baseProps} />);
-
+    renderWithTheme(
+      <BrowserRouter>
+        <PaymentNotice.Card {...baseProps} />
+      </BrowserRouter>
+    );
     expect(screen.getByText('Payee Icon')).toBeInTheDocument();
   });
 
   it('renders the ArrowForwardIosIcon', () => {
-    renderWithTheme(<PaymentNotice.Card {...baseProps} />);
+    renderWithTheme(
+      <BrowserRouter>
+        <PaymentNotice.Card {...baseProps} />
+      </BrowserRouter>
+    );
 
     expect(screen.getByTestId('ArrowForwardIosIcon')).toBeInTheDocument();
   });
