65ff1ae112e6abe0ef60bc787bfb32264725deb7
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2024-07-16 16:06:16 +0200

    [P4PU-210] last touches

--- src/components/Modals/PullPaymentsModal.tsx
+++ src/components/Modals/PullPaymentsModal.tsx
@@ -10,7 +10,7 @@ import {
   alpha
 } from '@mui/material';
 import React from 'react';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import { ArcRoutes } from 'routes/routes';
 import utils from 'utils';
@@ -38,6 +38,16 @@ const PullPaymentsModal = (props: { open: boolean }) => {
             <Stack spacing={2} width={'100%'}>
               <Typography variant="h4">{t('app.paymentNotices.optin.title')}</Typography>
               <Typography variant="body1">{t('app.paymentNotices.optin.body')}</Typography>
+              <Typography variant="body1">
+                {' '}
+                <Trans
+                  i18nKey={t('app.paymentNotices.optin.privacy')}
+                  components={{
+                    a1: <a href="#" target="_blank" />,
+                    a2: <a href="#" target="_blank" />
+                  }}
+                />
+              </Typography>
               <Stack pt={2} direction={'row'} spacing={2} justifyContent={'end'}>
                 <Button variant="outlined" size="large" color="primary" onClick={utils.modal.close}>
                   {t('app.routes.cancel')}
@@ -53,7 +63,7 @@ const PullPaymentsModal = (props: { open: boolean }) => {
                     }
                     utils.modal.close();
                   }}>
-                  {t('app.routes.continue')}
+                  {t('app.routes.consent')}
                 </Button>
               </Stack>
             </Stack>
--- src/utils/loaders.ts
+++ src/utils/loaders.ts
@@ -21,49 +21,62 @@ const getTransactionDetails = (id: string) =>
     }
   });
 
+// to be removed
+function resolveAfter<Type>(value: Type, delay: number): Promise<Type> {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(value), delay);
+  });
+}
+
+// to be removed
+const mockData = [
+  {
+    payee: {
+      name: 'Politecnico di Milano'
+    },
+    id: 1,
+    paymentInfo: 'RATA 1 - Anno Accademico 2023/2024',
+    amount: '171,00 €',
+    expiringDate: '31/01/2099'
+  },
+  {
+    payee: {
+      name: 'Comune di Milano'
+    },
+    id: 2,
+    paymentInfo: 'TARI 2024',
+    amount: '171,00 €',
+    expiringDate: '31/01/2099',
+    multiPayment: true
+  },
+  {
+    payee: {
+      name: 'Comune di Milano'
+    },
+    id: 3,
+    paymentInfo: 'Violazione CDS Verbale 0123456',
+    expiringDate: '31/01/2099',
+    amount: '28,70 €',
+    multiPayment: true
+  },
+  {
+    payee: {
+      name: 'Istituto d’Istruzione Superiore con un nome Molto Lungo che può andare su più righe'
+    },
+    id: 4,
+    paymentInfo: 'Iscrizione Anno Accademico 2023/2024',
+    amount: '171,00 €',
+    expiringDate: '31/01/2099'
+  }
+];
+
 const getPaymentNotices = () =>
   useQuery({
     queryKey: ['paymentNotices'],
-    queryFn: async () => [
-      {
-        payee: {
-          name: 'Politecnico di Milano'
-        },
-        id: 1,
-        paymentInfo: 'RATA 1 - Anno Accademico 2023/2024',
-        amount: '171,00 €',
-        expiringDate: '31/01/2099'
-      },
-      {
-        payee: {
-          name: 'Comune di Milano'
-        },
-        id: 2,
-        paymentInfo: 'TARI 2024',
-        amount: '171,00 €',
-        expiringDate: '31/01/2099',
-        multiPayment: true
-      },
-      {
-        payee: {
-          name: 'Comune di Milano'
-        },
-        id: 3,
-        paymentInfo: 'Violazione CDS Verbale 0123456',
-        expiringDate: '31/01/2099',
-        amount: '28,70 €',
-        multiPayment: true
-      },
-      {
-        payee: {
-          name: 'Istituto d’Istruzione Superiore con un nome Molto Lungo che può andare su più righe'
-        },
-        id: 4,
-        paymentInfo: 'Iscrizione Anno Accademico 2023/2024',
-        amount: '171,00 €',
-        expiringDate: '31/01/2099'
-      }
-    ]
+    queryFn: async () => {
+      const response = await resolveAfter(mockData, 500);
+      return response;
+    }
   });
 
 export default {
