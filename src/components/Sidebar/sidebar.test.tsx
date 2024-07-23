16b31b45734774dbba25c2faae5d8ca519b2986a
Michele Moio <michelemoiodev@gmail.com>
2024-07-23 23:35:52 +0200

    feat: [P4PU-207] sidebar responsiveness
    - moved sidebar styles in its own file
    - sidebar collapse/expand transitions
    - useCollapseMenu refactoring
    - useCollapseMenu state changes when crossing
      lg(1200px) threshold
    - unit tests

--- src/components/Sidebar/sidebar.test.tsx
+++ src/components/Sidebar/sidebar.test.tsx
@@ -5,43 +5,100 @@ import '../../translations/i18n';
 import React from 'react';
 import Sidebar from './index';
 import i18n from '../../translations/i18n';
+import { theme } from '@pagopa/mui-italia';
+import { ThemeProvider } from '@mui/material';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 void i18n.init({
   resources: {}
 });
 
 const SidebarWithRouter = () => (
-  <MemoryRouter>
-    <Sidebar />
-  </MemoryRouter>
+  <ThemeProvider theme={theme}>
+    <MemoryRouter>
+      <Sidebar />
+    </MemoryRouter>
+  </ThemeProvider>
 );
 
+jest.mock('@mui/material/useMediaQuery', () => jest.fn());
+
 describe('Sidebar component', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should render as expected', () => {
     render(<SidebarWithRouter />);
   });
 
-  test('renders with correct menu items', () => {
+  it('renders with correct menu items', () => {
     render(<SidebarWithRouter />);
 
     const hamburgerButton = screen.getByTestId('hamburgerButton');
 
     fireEvent.click(hamburgerButton);
     // Check if menu items are rendered
-    expect(screen.getByText('menu.homepage')).toBeTruthy();
-    expect(screen.getByText('menu.receipts')).toBeTruthy();
+    expect(screen.getByText('menu.homepage')).toBeInTheDocument();
+    expect(screen.getByText('menu.receipts')).toBeInTheDocument();
   });
 
-  test('toggles sidebar collapse/expand button is clicked', () => {
+  it('toggles sidebar collapse/expand button is clicked', () => {
     render(<SidebarWithRouter />);
 
     const hamburgerButton = screen.getByTestId('hamburgerButton');
-    expect(hamburgerButton).toBeTruthy();
+    expect(hamburgerButton).toBeInTheDocument();
 
     fireEvent.click(hamburgerButton);
-    expect(screen.getByText('menu.homepage')).toBeTruthy();
+    expect(screen.getByText('menu.homepage')).toBeInTheDocument();
 
     fireEvent.click(hamburgerButton);
-    expect(screen.queryByText('menu.homepage')).not.toBeTruthy();
+    expect(screen.queryByText('menu.homepage')).not.toBeInTheDocument();
+  });
+
+  it('renders with sidebar expanded on large screen', () => {
+    (useMediaQuery as jest.Mock).mockImplementation(() => true);
+    render(<SidebarWithRouter />);
+
+    expect(screen.getByLabelText('menu.navigationMenu')).toHaveAttribute('aria-expanded', 'true');
+  });
+
+  it('renders with sidebar collapsed on small screen', () => {
+    (useMediaQuery as jest.Mock).mockImplementation(() => false);
+    render(<SidebarWithRouter />);
+
+    const hamburgerButton = screen.getByTestId('hamburgerButton');
+    fireEvent.click(hamburgerButton);
+    expect(screen.getByText('menu.homepage')).toBeInTheDocument();
+  });
+
+  it('renders close icon and handles click to collapse', () => {
+    (useMediaQuery as jest.Mock).mockImplementation(() => false);
+    render(<SidebarWithRouter />);
+
+    const hamburgerButton = screen.getByTestId('hamburgerButton');
+    fireEvent.click(hamburgerButton);
+
+    const closeIcon = screen.getByTestId('collapseClose');
+    expect(closeIcon).toBeInTheDocument();
+
+    fireEvent.click(closeIcon);
+
+    // Check if the sidebar is collapsed by verifying the absence of menu items
+    expect(screen.queryByText('menu.homepage')).not.toBeInTheDocument();
+  });
+
+  it('collapses when clicking on an item on lower resolutions', () => {
+    (useMediaQuery as jest.Mock).mockImplementation(() => false);
+    render(<SidebarWithRouter />);
+
+    const hamburgerButton = screen.getByTestId('hamburgerButton');
+    fireEvent.click(hamburgerButton);
+
+    const home = screen.getByText('menu.homepage');
+    fireEvent.click(home);
+
+    const navigationMenu = screen.getByLabelText('menu.navigationMenu');
+    expect(navigationMenu).toHaveAttribute('aria-expanded', 'false');
   });
 });
