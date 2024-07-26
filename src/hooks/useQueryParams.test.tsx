d8d62df3d4ba29b040e72bf1e263968c3c469c50
Vito Falco <vitofalco@gmail.com>
2024-07-26 10:53:28 +0200

    Add route. Add new hook to manage query param.

---
+++ src/hooks/useQueryParams.test.tsx
@@ -0,0 +1,22 @@
+import '@testing-library/jest-dom';
+import { renderHook } from '@testing-library/react';
+import useQueryParams from './useQueryParams';
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useLocation: () => new URL('http://dummysite.it/login?state=test123&code=CODE123')
+}));
+
+describe('useQueryParams hook', () => {
+  it('should return the params called `state` and `code`', () => {
+    const { result } = renderHook(useQueryParams);
+    const { state, code } = result.current;
+    expect(state).toBe('test123');
+    expect(code).toBe('CODE123');
+  });
+  it('should return an undefined param called `greeting`', () => {
+    const { result } = renderHook(useQueryParams);
+    const { greeting } = result.current;
+    expect(greeting).toBe(undefined);
+  });
+});
