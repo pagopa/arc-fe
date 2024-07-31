202a80716682fcb802ea64d98bc5c152a6a92605
anislucidi01 <anis.lucidi@pagopa.it>
2024-07-31 12:47:27 +0200

    P4PU-297: Made it so that the hamburger icon is always at the bottom of the page.

--- src/components/Sidebar/sidebar.styles.ts
+++ src/components/Sidebar/sidebar.styles.ts
@@ -6,10 +6,10 @@ export const sidebarStyles = (theme: Theme, collapsed: boolean): Record<string, 
     position: collapsed ? 'relative' : 'fixed',
     width: '100%',
     top: 0,
-    height: '100%',
+    height: '100vh',
     transition: 'width 0.3s ease, height 0.3s ease', // Add transition for smooth resizing
     [theme.breakpoints.between('sm', 'lg')]: { width: collapsed ? '100%' : 'fit-content' },
-    [theme.breakpoints.up('lg')]: { width: 'fit-content', position: 'relative' },
+    [theme.breakpoints.up('lg')]: { width: 'fit-content', position: 'sticky' },
     [theme.breakpoints.down('lg')]: { height: collapsed ? 'fit-content' : '100%' }
   },
   nav: {
@@ -42,6 +42,8 @@ export const sidebarStyles = (theme: Theme, collapsed: boolean): Record<string, 
   },
   hamburgerBox: {
     marginTop: 'auto',
+    position: 'sticky',
+    bottom: '0',
     transition: 'opacity 0.3s ease', // Add transition for smooth visibility change
     [theme.breakpoints.down('lg')]: {
       marginTop: collapsed ? 0 : 'auto',
