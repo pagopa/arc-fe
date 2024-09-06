f5587b417fe289a37d02e41204910f6aaaed0df2
Vito Falco <vitofalco@gmail.com>
2024-09-06 08:54:50 +0200

    feat: [P4PU-369] Refact assistance form (#139)
    Co-authored-by: Ivan Di Luccio <ivan.diluccio@pagopa.it>

--- src/components/AssistanceForm/AssistanceForm.test.tsx
+++ src/components/AssistanceForm/AssistanceForm.test.tsx
@@ -1,9 +1,45 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import { AssistanceForm } from './index';
 import '@testing-library/jest-dom';
+import utils from 'utils';
+
+jest.mock('utils', () => ({
+  loaders: {
+    getUserInfo: jest.fn()
+  },
+  apiClient: {
+    token: {
+      getZendeskAssistanceToken: jest.fn()
+    }
+  }
+}));
 
 describe('AssistanceForm Component', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('renders having e-mail of connected user', async () => {
+    const mockUserData = {
+      data: {
+        userId: '_1a',
+        fiscalCode: 'TINIT-PLOMRC01P30L221D',
+        familyName: 'Polo',
+        name: 'Marco',
+        email: 'ilmilione@virgilio.it'
+      }
+    };
+    (utils.loaders.getUserInfo as jest.Mock).mockReturnValue(mockUserData);
+
+    render(<AssistanceForm />);
+
+    expect(utils.loaders.getUserInfo).toHaveBeenCalledTimes(1);
+
+    const email = screen.getByTestId('confirm-email');
+    expect(email).toHaveAttribute('value', mockUserData.data.email);
+  });
+
   it('renders having confirm button disabled', () => {
     render(<AssistanceForm />);
     const confirmButton = screen.getByTestId('assistance-confirm-button');
@@ -27,5 +63,38 @@ describe('AssistanceForm Component', () => {
 
     fireEvent.change(confirmEmail, { target: { value: 'pippo@pippo.it' } });
     expect(confirmButton).not.toHaveClass('Mui-disabled');
+  });
+
+  it('confirm email and obtain a zendesk jwt', async () => {
+    const mockGetZendeskAssistanceToken = {
+      data: {
+        assistanceToken: 'z123',
+        returnTo: 'https://www.pagopa.it'
+      }
+    };
+    (utils.apiClient.token.getZendeskAssistanceToken as jest.Mock).mockReturnValue(
+      mockGetZendeskAssistanceToken
+    );
+
+    render(<AssistanceForm />);
+
+    const confirmButton = screen.getByTestId('assistance-confirm-button');
+    const email = screen.getByTestId('confirm-email');
+    const confirmEmail = screen.getByTestId('assistance-confirm-email');
+    const jwtStringInput = screen.getByTestId('jwtString');
+    const returnToInput = screen.getByTestId('returnTo');
+
+    fireEvent.change(email, { target: { value: 'pippo@pippo.it' } });
+    fireEvent.change(confirmEmail, { target: { value: 'pippo@pippo.it' } });
+    fireEvent.click(confirmButton);
+
+    expect(utils.apiClient.token.getZendeskAssistanceToken).toHaveBeenCalledTimes(1);
+    waitFor(() => {
+      expect(jwtStringInput).toHaveAttribute(
+        'value',
+        mockGetZendeskAssistanceToken.data.assistanceToken
+      );
+      expect(returnToInput).toHaveAttribute('value', mockGetZendeskAssistanceToken.data.returnTo);
+    });
   });
 });
--- src/components/AssistanceForm/AssistanceForm.tsx
+++ src/components/AssistanceForm/AssistanceForm.tsx
@@ -7,21 +7,35 @@ import {
   TextField,
   Typography,
   useTheme,
-  Link
+  Link,
+  Box
 } from '@mui/material';
 import { Trans, useTranslation } from 'react-i18next';
 import { ArrowBack } from '@mui/icons-material';
 import utils from 'utils';
+import { ZendeskAssistanceTokenResponse } from '../../../generated/data-contracts';
 
 export const AssistanceForm = () => {
   const { t } = useTranslation();
   const theme = useTheme();
-  const [email, setEmail] = useState('');
+  const { data } = utils.loaders.getUserInfo();
+  const [email, setEmail] = useState(data?.email || '');
   const [emailError, setEmailError] = useState(false);
+  const [zendeskData, setZendeskData] = useState<ZendeskAssistanceTokenResponse>();
   const [emailConfirmError, setEmailConfirmError] = useState(false);
   const [emailConfirm, setEmailConfirm] = useState('');
   const reg = new RegExp('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$');
   const [active, setActive] = useState(false);
+
+  async function getAssistanceJWT(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    e.preventDefault();
+    const { data: zendeskAssistance } = await utils.apiClient.token.getZendeskAssistanceToken({
+      userEmail: email
+    });
+    const form = document.getElementById('jwtForm') as HTMLFormElement;
+    setZendeskData(zendeskAssistance);
+    form.submit();
+  }
 
   useEffect(() => {
     if (email) setEmailError(!reg.test(email));
@@ -29,102 +43,129 @@ export const AssistanceForm = () => {
     setActive(reg.test(email) && emailConfirm === email);
   }, [email, emailConfirm]);
 
+  useEffect(() => {
+    setEmail(data?.email || '');
+  }, [data?.email]);
+
   return (
     <>
-      <Stack spacing={3}>
-        <Stack
-          flex={1}
-          direction={{ xs: 'column', sm: 'column' }}
-          justifyContent={'space-between'}
-          gap={2}
-          mb={5}>
-          <Typography variant="h3">{t('app.assistance.title')}</Typography>
+      <form>
+        <Stack spacing={3}>
+          <Stack
+            flex={1}
+            direction={{ xs: 'column', sm: 'column' }}
+            justifyContent={'space-between'}
+            gap={2}
+            mb={5}>
+            <Typography variant="h3">{t('app.assistance.title')}</Typography>
 
-          <Typography variant="body1">{t('app.assistance.description')}</Typography>
-        </Stack>
-        <Card
-          sx={{
-            padding: 2,
-            width: '100%'
-          }}>
-          <CardActions>
-            <Stack spacing={2} width={'100%'}>
-              <Typography variant="h4">{t('app.assistance.cardTitle')}</Typography>
-              <Typography variant="body1">{t('app.assistance.cardDescription')}</Typography>
-              <Stack spacing={3}>
-                <TextField
-                  inputProps={{ 'data-testid': 'confirm-email' }}
-                  role="textbox"
-                  aria-required="false"
-                  value={email}
-                  aria-label={t('app.assistance.input1Placeholder')}
-                  size="small"
-                  error={emailError}
-                  helperText={emailError && t('app.assistance.input1Helper')}
-                  onChange={(e) => {
-                    setEmail(e.target.value);
-                  }}
-                  sx={{ maxWidth: { lg: '20vw', md: '25vw', sm: '35vw', xs: '100%' } }}
-                  label={t('app.assistance.input1Placeholder')}
-                  InputLabelProps={{
-                    sx: {
-                      color: theme.palette.text.secondary,
-                      fontWeight: 600
-                    }
-                  }}
-                />
-                <TextField
-                  inputProps={{ 'data-testid': 'assistance-confirm-email' }}
-                  role="textbox"
-                  aria-required="false"
-                  value={emailConfirm}
-                  error={emailConfirmError}
-                  aria-label={t('app.assistance.input2Placeholder')}
-                  onChange={(e) => {
-                    setEmailConfirm(e.target.value);
-                  }}
-                  size="small"
-                  sx={{ maxWidth: { lg: '20vw', md: '25vw', sm: '35vw', xs: '100%' } }}
-                  label={t('app.assistance.input2Placeholder')}
-                  helperText={emailConfirmError && t('app.assistance.input2Helper')}
-                  InputLabelProps={{
-                    sx: {
-                      color: theme.palette.text.secondary,
-                      fontWeight: 600
-                    }
-                  }}
-                />
+            <Typography variant="body1">{t('app.assistance.description')}</Typography>
+          </Stack>
+          <Card
+            sx={{
+              padding: 2,
+              width: '100%'
+            }}>
+            <CardActions>
+              <Stack spacing={2} width={'100%'}>
+                <Typography variant="h4">{t('app.assistance.cardTitle')}</Typography>
+                <Typography variant="body1">{t('app.assistance.cardDescription')}</Typography>
+                <Stack spacing={3}>
+                  <TextField
+                    inputProps={{ 'data-testid': 'confirm-email' }}
+                    role="textbox"
+                    aria-required="false"
+                    value={email}
+                    aria-label={t('app.assistance.input1Placeholder')}
+                    size="small"
+                    error={emailError}
+                    helperText={emailError && t('app.assistance.input1Helper')}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                    sx={{ maxWidth: { lg: '20vw', md: '25vw', sm: '35vw', xs: '100%' } }}
+                    label={t('app.assistance.input1Placeholder')}
+                    InputLabelProps={{
+                      sx: {
+                        color: theme.palette.text.secondary,
+                        fontWeight: 600
+                      }
+                    }}
+                  />
+                  <TextField
+                    inputProps={{ 'data-testid': 'assistance-confirm-email' }}
+                    role="textbox"
+                    aria-required="false"
+                    value={emailConfirm}
+                    error={emailConfirmError}
+                    aria-label={t('app.assistance.input2Placeholder')}
+                    onChange={(e) => {
+                      setEmailConfirm(e.target.value);
+                    }}
+                    size="small"
+                    sx={{ maxWidth: { lg: '20vw', md: '25vw', sm: '35vw', xs: '100%' } }}
+                    label={t('app.assistance.input2Placeholder')}
+                    helperText={emailConfirmError && t('app.assistance.input2Helper')}
+                    InputLabelProps={{
+                      sx: {
+                        color: theme.palette.text.secondary,
+                        fontWeight: 600
+                      }
+                    }}
+                  />
+                </Stack>
               </Stack>
-            </Stack>
-          </CardActions>
-        </Card>
-        <Typography variant="body1">
-          <Trans
-            i18nKey={t('app.assistance.privacy')}
-            components={{
-              link1: <Link href="#" fontWeight={800} />
-            }}
-          />
-        </Typography>
-        <Stack direction={'row'} justifyContent={'space-between'}>
-          <Button
-            variant="outlined"
-            size="large"
-            color="primary"
-            startIcon={<ArrowBack />}
-            onClick={() => utils.modal.open(utils.modal.ModalId.ASSISTANCEBACK)}>
-            {t('app.routes.exit')}
-          </Button>
-          <Button
-            data-testid="assistance-confirm-button"
-            variant="contained"
-            size="large"
-            href={utils.config.assistanceLink}
-            disabled={!active}>
-            {t('app.assistance.confirm')}
-          </Button>
+            </CardActions>
+          </Card>
+          <Typography variant="body1">
+            <Trans
+              i18nKey={t('app.assistance.privacy')}
+              components={{
+                link1: <Link href="#" fontWeight={800} />
+              }}
+            />
+          </Typography>
+          <Stack direction={'row'} justifyContent={'space-between'}>
+            <Button
+              variant="outlined"
+              size="large"
+              color="primary"
+              startIcon={<ArrowBack />}
+              onClick={() => utils.modal.open(utils.modal.ModalId.ASSISTANCEBACK)}>
+              {t('app.routes.exit')}
+            </Button>
+            <Button
+              type="submit"
+              data-testid="assistance-confirm-button"
+              variant="contained"
+              size="large"
+              onClick={(e) => getAssistanceJWT(e)}
+              disabled={!active}>
+              {t('app.assistance.confirm')}
+            </Button>
+          </Stack>
         </Stack>
-      </Stack>
+      </form>
+      <Box display="none">
+        <form id="jwtForm" method="POST" action="https://pagopa.zendesk.com/access/jwt">
+          <input
+            readOnly
+            type="hidden"
+            id="jwtString"
+            data-testid="jwtString"
+            name="jwt"
+            value={(zendeskData && zendeskData.assistanceToken.trim()) || ''}
+          />
+          <input
+            readOnly
+            type="hidden"
+            id="returnTo"
+            data-testid="returnTo"
+            name="return_to"
+            value={(zendeskData && zendeskData.returnTo.trim()) || ''}
+          />
+        </form>
+      </Box>
     </>
   );
 };
