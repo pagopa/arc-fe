7471ebf36b67d9776f603cd6542050947bda6bcc
Vito Falco <vitofalco@gmail.com>
2024-10-15 15:39:21 +0200

    Manage unhappy for login. Manage different message for courtesy

--- src/routes/AuthCallback/index.tsx
+++ src/routes/AuthCallback/index.tsx
@@ -3,13 +3,21 @@ import { useLoaderData } from 'react-router-dom';
 import { ArcRoutes } from '../routes';
 import { TokenResponse } from '../../../generated/data-contracts';
 import { Box, CircularProgress } from '@mui/material';
+import { tokenResponseSchema } from '../../../generated/zod-schema';
 
 export default function AuthCallback() {
-  const result = useLoaderData() as TokenResponse | null;
-  if (result) {
+  const result = useLoaderData() as TokenResponse | number;
+  const checkToken = tokenResponseSchema.safeParse(result);
+
+  if (checkToken.success) {
+    // if we have a formal token
     window.localStorage.setItem('accessToken', (result as TokenResponse).accessToken);
     window.location.replace(ArcRoutes.DASHBOARD);
+  } else if (typeof result === 'number') {
+    // if we have an error code
+    window.location.replace(`${ArcRoutes.COURTESY_PAGE}?errorcode=${result}`);
   } else {
+    // otherwhise
     window.location.replace(ArcRoutes.LOGIN);
   }
 
--- src/routes/CourtesyPage/index.tsx
+++ src/routes/CourtesyPage/index.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import { Button, Typography, Container, Box } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { useSearchParams, Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 export const CourtesyPage = () => {
+  const { t } = useTranslation();
+  const [searchParams] = useSearchParams();
+  const errorMessage = searchParams.get('errorcode') || 'default';
+
   return (
     <Container maxWidth="sm">
       <Box textAlign="center" mt={10}>
-        <Typography variant="h4" gutterBottom>
-          Sorry, the page you're looking for does not exist.
+        <Typography variant="h4" gutterBottom data-testid="courtesyPage.title">
+          {t(`courtesyPage.${errorMessage}.title`)}
         </Typography>
-        <Typography variant="body1" paragraph>
-          Please navigate back to the homepage or try a different page.
+        <Typography variant="body1" paragraph data-testid="courtesyPage.body">
+          {t(`courtesyPage.${errorMessage}.body`)}
         </Typography>
-        <Button component={Link} to="/" variant="contained" color="primary">
-          Go to Home
+        <Button
+          component={Link}
+          to="/"
+          variant="contained"
+          color="primary"
+          data-testid="courtesyPage.cta">
+          {t(`courtesyPage.${errorMessage}.cta`)}
         </Button>
       </Box>
     </Container>
