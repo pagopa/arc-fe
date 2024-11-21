2c3fee9687fa9dc4337addeb331d3bcdcb059b75
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2024-11-21 11:18:36 +0100

    [P4PU-695] updating github environments variables and secrets by terraform

--- .identity/99_locals.tf
+++ .identity/99_locals.tf
@@ -7,19 +7,13 @@ locals {
 
   repo_secrets = var.env_short == "p" ? {
     SONAR_TOKEN = data.azurerm_key_vault_secret.sonar_token[0].value
-  } : {}
-
-  env_variables = contains(["d", "u"], var.env_short) ? {
-    API_HOST              = "https://api.${var.env}.cittadini.pagopa.it/arc/v1"
-    CHECKOUT_HOST         = "https://${var.env}.checkout.pagopa.it"
-    CHECKOUT_PLATFORM_URL = "https://api.${var.env}.platform.pagopa.it/checkout/ec/v1"
-    LOGIN_URL             = "https://api.${var.env}.cittadini.pagopa.it/arc/v1/login/oneidentity"
-  } : {}
-
-  env_secrets = {
     E2E_USERNAME = try(data.azurerm_key_vault_secret.username_test[0].value, "")
     E2E_PASSWORD = try(data.azurerm_key_vault_secret.password_test[0].value, "")
-  }
+  } : {}
+
+  env_variables = contains(["d", "u"], var.env_short) ? {} : {}
+
+  env_secrets = contains(["d", "u"], var.env_short) ? {} : {}
 
   map_repo = {
     "dev" : "*",
