52fb19ff2b78a48952c15d47cb353aceeac30ced
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2024-11-22 12:41:48 +0100

    [P4PU-708] adding a new tsc config file

--- src/components/HealthCheck/HealthCheck.tsx
+++ src/components/HealthCheck/HealthCheck.tsx
@@ -3,19 +3,15 @@ import utils from 'utils';
 
 export const HealthCheck = () => {
   useEffect(() => {
-    if (utils.config.env === 'LOCAL') {
-      const checkHealth = async () => {
-        // apiClient doesn't expose a testing endpoit anymore
-        // but I like the idea to have a this kind of component to setup or check
-        // important stuffs in the future
-        // await utils.apiClient.info.healthCheck();
-      };
-      checkHealth();
-    } else {
-      // dont't remove this console log, it's useful to debug and have a feedback about the running version
-      console.log(`ARC FE version: ${utils.config.version}`);
-    }
+    const checkHealth = async () => {
+      // apiClient doesn't expose a testing endpoit anymore
+      // but I like the idea to have this kind of component to setup or check
+      // important stuffs in the future
+      // await utils.apiClient.info.healthCheck();
+    };
+    checkHealth();
+    // dont't remove this console log, it's useful to debug and have a feedback about the running version
+    console.log(`ARC FE version: ${utils.config.version}`);
   }, []);
-
   return null; // This component does not render anything
 };
