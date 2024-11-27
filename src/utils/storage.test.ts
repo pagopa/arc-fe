c87e3d40a049202aca4e2f70f3462802a9fac38c
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2024-11-27 18:11:31 +0100

    [P4PU-745] added unit tests

--- src/utils/storage.test.ts
+++ src/utils/storage.test.ts
@@ -1,11 +1,11 @@
 import storage from './storage';
 
-describe('storage.user.logOut', () => {
+describe('storage', () => {
   afterEach(() => {
     vi.clearAllMocks();
   });
 
-  it('should clear sessionStorage and localStorage', () => {
+  it('user.logot should clear sessionStorage and localStorage', () => {
     // Mock the sessionStorage and localStorage
     const storageSpy = vi.spyOn(Storage.prototype, 'clear');
 
@@ -13,5 +13,17 @@ describe('storage.user.logOut', () => {
 
     // Assert that the clear methods were called
     expect(storageSpy).toHaveBeenCalled();
+  });
+
+  it('user.setToken and user.getToken and should set and get the accessToken item', () => {
+    const setItemSpy = vi.spyOn(Storage.prototype, 'setItem');
+    const getItemSpy = vi.spyOn(Storage.prototype, 'getItem');
+
+    expect(storage.user.hasToken()).not.toBeTruthy();
+    const token = storage.user.setToken('testToken');
+    expect(setItemSpy).toHaveBeenCalled();
+    expect(token).toBe('testToken');
+    expect(storage.user.hasToken()).toBeTruthy();
+    expect(getItemSpy).toHaveBeenCalled();
   });
 });
