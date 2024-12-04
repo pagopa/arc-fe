b38b4e94eaf9a28ae0922e70709581a6267dce71
Michele Moio <michelemoiodev@gmail.com>
2024-12-04 06:41:20 +0100

    fix: [P4PU-729] removes modal links (#210)

--- src/components/Modals/PullPaymentsModal.tsx
+++ src/components/Modals/PullPaymentsModal.tsx
@@ -10,7 +10,7 @@ import {
   alpha
 } from '@mui/material';
 import React from 'react';
-import { Trans, useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import { ArcRoutes } from 'routes/routes';
 import utils from 'utils';
@@ -40,16 +40,6 @@ const PullPaymentsModal = (props: { open: boolean }) => {
             <Stack spacing={2} width={'100%'}>
               <Typography variant="h4">{t('app.paymentNotices.optin.title')}</Typography>
               <Typography variant="body1">{t('app.paymentNotices.optin.body')}</Typography>
-              <Typography variant="body1">
-                {' '}
-                <Trans
-                  i18nKey={t('app.paymentNotices.optin.privacy')}
-                  components={{
-                    a1: <a href="#" target="_blank" />,
-                    a2: <a href="#" target="_blank" />
-                  }}
-                />
-              </Typography>
               <Stack pt={2} direction={'row'} spacing={2} justifyContent={'end'}>
                 <Button
                   variant="outlined"
