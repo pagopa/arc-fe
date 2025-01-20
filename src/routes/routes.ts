fad8e928d5f583fce825a9cdfebaba7c9d2e9913
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2025-01-20 15:00:38 +0100

    [P4PU-758] updating routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,14 +3,14 @@ const deployPath = config.deployPath;
 
 export const ArcRoutes = {
   DASHBOARD: `${deployPath}/`,
-  TRANSACTION: `${deployPath}/transactions/:id`,
-  TRANSACTIONS: `${deployPath}/transactions/`,
-  PAYMENT_NOTICES: `${deployPath}/payment-notices/`,
-  USER: `${deployPath}/user`,
-  COURTESY_PAGE: `${deployPath}/courtesy`,
-  LOGIN: `${deployPath}/login`,
-  ASSISTANCE: `${deployPath}/assistance`,
-  PAYMENT_NOTICE_DETAIL: `${deployPath}/payment-notices/:id/:paTaxCode`,
+  TRANSACTION: `${deployPath}/ricevute/:id`,
+  TRANSACTIONS: `${deployPath}/ricevute/`,
+  PAYMENT_NOTICES: `${deployPath}/avvisi/`,
+  PAYMENT_NOTICE_DETAIL: `${deployPath}/avvisi/:id/:paTaxCode`,
+  USER: `${deployPath}/profilo`,
+  COURTESY_PAGE: `${deployPath}/errore`,
+  LOGIN: `${deployPath}/accesso`,
+  ASSISTANCE: `${deployPath}/assistenza`,
   AUTH_CALLBACK: `${deployPath}/auth-callback`,
   PRIVACY_PAGE: `${deployPath}/privacy_page`,
   RESOURCES: `${deployPath}/resources`
