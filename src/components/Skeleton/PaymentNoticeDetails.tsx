18769c3b001bb47b9cf9d71f1ec1480908249456
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2025-02-04 16:46:27 +0100

    [P4PU-833] add PaymentNoticeDetails skeleton and enhance TransactionDetails layout

--- src/components/Skeleton/PaymentNoticeDetails.tsx
+++ src/components/Skeleton/PaymentNoticeDetails.tsx
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 import { Grid, Card, CardContent, Skeleton, Stack, Box, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 
-const TransactionDetails = () => {
+const PaymentNoticeDetails = () => {
   const { t } = useTranslation();
   return (
     <>
@@ -11,28 +11,10 @@ const TransactionDetails = () => {
         fontSize={{ xs: 28, md: 32 }}
         sx={{ wordBreak: 'break-word' }}
         mb={3}>
-        {t('app.transactionDetail.title')}
+        {t('app.paymentNoticeDetail.title')}
       </Typography>
       <Grid container spacing={4}>
         <Grid item xs={12} md={7}>
-          <Card>
-            <CardContent sx={{ padding: 4 }}>
-              <Skeleton variant="rounded" sx={{ marginBottom: 4 }} width={'50%'} height={30} />
-              <Stack spacing={2} mb={4}>
-                {Array.from({ length: 2 }, (_, i) => (
-                  <Fragment key={i}>
-                    <Skeleton variant="rounded" height={12} />
-                    <Skeleton variant="rounded" height={12} />
-                    <Skeleton variant="rounded" height={12} width={'75%'} />
-                  </Fragment>
-                ))}
-              </Stack>
-              <Skeleton variant="rounded" width={'25%'} height={21} />
-            </CardContent>
-          </Card>
-        </Grid>
-
-        <Grid item xs={12} md={5}>
           <Card>
             <CardContent sx={{ padding: 4 }}>
               <Skeleton variant="rounded" sx={{ marginBottom: 4 }} width={'50%'} height={30} />
@@ -53,9 +35,27 @@ const TransactionDetails = () => {
             </CardContent>
           </Card>
         </Grid>
+
+        <Grid item xs={12} md={5}>
+          <Card>
+            <CardContent sx={{ padding: 4 }}>
+              <Skeleton variant="rounded" sx={{ marginBottom: 4 }} width={'50%'} height={30} />
+              <Stack spacing={2} mb={4}>
+                {Array.from({ length: 2 }, (_, i) => (
+                  <Fragment key={i}>
+                    <Skeleton variant="rounded" height={12} />
+                    <Skeleton variant="rounded" height={12} />
+                    <Skeleton variant="rounded" height={12} width={'75%'} />
+                  </Fragment>
+                ))}
+              </Stack>
+              <Skeleton variant="rounded" width={'25%'} height={21} />
+            </CardContent>
+          </Card>
+        </Grid>
       </Grid>
     </>
   );
 };
 
-export default TransactionDetails;
+export default PaymentNoticeDetails;
--- src/components/Skeleton/index.ts
+++ src/components/Skeleton/index.ts
@@ -1,11 +1,13 @@
 import TransactionListSkeleton from './TransactionsList';
 import TransactionDetailsSkeleton from './TransactionDetails';
 import PaymentNoticesListSkeleton from './PaymentNoticesList';
+import PaymentNoticeDetails from './PaymentNoticeDetails';
 import UserInfoSkeleton from './UserInfo';
 
 export {
   TransactionListSkeleton,
   TransactionDetailsSkeleton,
   PaymentNoticesListSkeleton,
+  PaymentNoticeDetails,
   UserInfoSkeleton
 };
