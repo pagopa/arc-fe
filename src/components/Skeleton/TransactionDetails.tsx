7d89c83d7b347f7826e2c21db085f9b22cb9284c
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2025-02-05 10:05:03 +0100

    [P4PU-833] extract skeleton components into DetailsSkeleton for reusability

--- src/components/Skeleton/TransactionDetails.tsx
+++ src/components/Skeleton/TransactionDetails.tsx
@@ -1,61 +1,8 @@
-import React, { Fragment } from 'react';
-import { Grid, Card, CardContent, Skeleton, Stack, Box, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import DetailsSkeleton from './detailsSkeleton';
 
 const TransactionDetails = () => {
   const { t } = useTranslation();
-  return (
-    <>
-      <Typography
-        variant="h2"
-        fontSize={{ xs: 28, md: 32 }}
-        sx={{ wordBreak: 'break-word' }}
-        mb={3}>
-        {t('app.transactionDetail.title')}
-      </Typography>
-      <Grid container spacing={4}>
-        <Grid item xs={12} md={7}>
-          <Card>
-            <CardContent sx={{ padding: 4 }}>
-              <Skeleton variant="rounded" sx={{ marginBottom: 4 }} width={'50%'} height={30} />
-              <Stack spacing={2} mb={4}>
-                {Array.from({ length: 2 }, (_, i) => (
-                  <Fragment key={i}>
-                    <Skeleton variant="rounded" height={12} />
-                    <Skeleton variant="rounded" height={12} />
-                    <Skeleton variant="rounded" height={12} width={'75%'} />
-                  </Fragment>
-                ))}
-              </Stack>
-              <Skeleton variant="rounded" width={'25%'} height={21} />
-            </CardContent>
-          </Card>
-        </Grid>
-
-        <Grid item xs={12} md={5}>
-          <Card>
-            <CardContent sx={{ padding: 4 }}>
-              <Skeleton variant="rounded" sx={{ marginBottom: 4 }} width={'50%'} height={30} />
-              <Stack spacing={4} mb={4}>
-                {Array.from({ length: 7 }, (_, i) => (
-                  <Box key={i}>
-                    <Skeleton
-                      variant="rounded"
-                      height={12}
-                      width={'50%'}
-                      sx={{ marginBottom: 2 }}
-                    />
-                    <Skeleton variant="rounded" height={21} width={'75%'} />
-                  </Box>
-                ))}
-              </Stack>
-              <Skeleton variant="rounded" width={'25%'} height={21} />
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
-    </>
-  );
+  return <DetailsSkeleton title={t('app.transactionDetail.title')} direction="row-reverse" />;
 };
-
 export default TransactionDetails;
