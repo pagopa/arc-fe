6eb3aa1a957a9c8116c1e1f4bb023d3337e7474f
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2025-03-04 15:17:58 +0100

    [P4PU-885] remove jest.config.ts and update CookieBanner test for environment handling

--- src/components/CookieBanner/CookieBanner.test.tsx
+++ src/components/CookieBanner/CookieBanner.test.tsx
@@ -1,10 +1,14 @@
+/**
+ * @vitest-environment-options { "url": "https://example.com/" }
+ */
 import React from 'react';
 import { render } from '@testing-library/react';
 import CookieBanner from '.';
 import '@testing-library/jest-dom';
 
 describe('CookieBanner component', () => {
-  it('should render nothing without causing error', () => {
+  it('should render nothing without causing error', async () => {
+    process.env.ENV = 'DEV';
     const { container } = render(<CookieBanner />);
     expect(container).toBeEmptyDOMElement();
   });
