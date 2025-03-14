fca04f8eb4d1681ef67807efd24fb8de0be01263
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2025-03-14 16:47:33 +0100

    [P4PU-898]: remove unused imports in Login component tests

--- src/routes/Login/Login.test.tsx
+++ src/routes/Login/Login.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Login from '.';
 import { BrowserRouter } from 'react-router-dom';
 import '@testing-library/jest-dom';
