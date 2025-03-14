b2d0cae57b33a76ada5bfa11a670785b09d8920c
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2025-03-14 15:57:20 +0100

    feat: implement payment configuration step with amount and causale inputs

--- src/components/Spontanei/Form.tsx
+++ src/components/Spontanei/Form.tsx
@@ -12,10 +12,20 @@ const Spontanei = () => {
   const [step, setStep] = React.useState(0);
   const [ente, setEnte] = React.useState<string | null>(null);
   const [servizio, setServizio] = React.useState<Servizio | null>(null);
+  const [amount, setAmount] = React.useState<number>(0);
+  const [causale, setCausale] = React.useState('');
+
   const { t } = useTranslation();
   const navigate = useNavigate();
 
   const onContinue = () => {
+    if (step === 1 && servizio === 'Bollo Auto') {
+      window.open('https://www.tributi.regione.lombardia.it/PagoBollo/#/', '');
+      return;
+    }
+    if (step === 2 && (!causale || !amount)) {
+      return;
+    }
     setStep(step + 1);
   };
 
@@ -25,9 +35,16 @@ const Spontanei = () => {
   };
 
   useEffect(() => {
-    setEnte(null);
-    setServizio(null);
+    if (servizio === 'Rinnovo Licenza Caccia') return setAmount(200);
+    setAmount(0);
+  }, [servizio]);
+
+  useEffect(() => {
+    if (step == 1) setCausale('');
   }, [step]);
+
+  const OnAmountChange =
+    servizio !== 'Rinnovo Licenza Caccia' ? (amount: number) => setAmount(amount) : undefined;
 
   return (
     <Stack>
@@ -37,7 +54,13 @@ const Spontanei = () => {
         <Steps activeStep={step} />
         {step === 0 && <SelezionaEnte setEnte={setEnte} />}
         {step === 1 && <SelezionaServizio setServizio={setServizio} />}
-        {step === 2 && <ConfiguraPagamento />}
+        {step === 2 && (
+          <ConfiguraPagamento
+            onCausaleChange={setCausale}
+            amount={amount}
+            onAmountChange={OnAmountChange}
+          />
+        )}
         <Stack direction="row" justifyContent={'space-between'}>
           <Button variant="outlined" onClick={onBack} startIcon={<ArrowBack />}>
             {step === 0 ? t('spontanei.form.abort') : t('spontanei.form.back')}
@@ -45,7 +68,11 @@ const Spontanei = () => {
           <Button
             variant="contained"
             onClick={onContinue}
-            disabled={(step === 0 && !ente) || (step == 1 && !servizio)}>
+            disabled={
+              (step === 0 && !ente) ||
+              (step == 1 && !servizio) ||
+              (step == 2 && (!causale || !amount))
+            }>
             {t('spontanei.form.continue')}
           </Button>
         </Stack>
--- src/components/Spontanei/steps/Configura.tsx
+++ src/components/Spontanei/steps/Configura.tsx
@@ -1,5 +1,41 @@
+import { Card, Stack, TextField, Typography } from '@mui/material';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
-const ConfiguraPagamento = () => <>ConfiguraPagamento</>;
+interface Configura {
+  onAmountChange?: (amount: number) => void;
+  amount?: number;
+  onCausaleChange?: (casuale: string) => void;
+}
+
+const ConfiguraPagamento = (props: Configura) => {
+  const { onAmountChange, amount = 0, onCausaleChange } = props;
+  const { t } = useTranslation();
+
+  return (
+    <Card variant="outlined">
+      <Stack spacing={2} padding={4}>
+        <Typography variant="h6">{t('spontanei.form.steps.step3.title')}</Typography>
+        <Typography>{t('spontanei.form.steps.step3.description')}</Typography>
+        <Stack direction="row" justifyContent={'space-between'} spacing={2}>
+          <TextField
+            label="Importo (€)"
+            variant="outlined"
+            type="number"
+            value={amount}
+            disabled={!onAmountChange}
+            onChange={(e) => onAmountChange?.(parseInt(e.target.value, 10))}
+          />
+          <TextField
+            label="Causale"
+            variant="outlined"
+            onChange={(e) => onCausaleChange?.(e.target.value)}
+            sx={{ width: '-webkit-fill-available' }}
+          />
+        </Stack>
+      </Stack>
+    </Card>
+  );
+};
 
 export default ConfiguraPagamento;
