1dffff13f397c113598c03746912bfe92f6227c8
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2025-03-18 16:49:41 +0100

    [P4PU-854] update Riepilogo component to display payment notice details and modify translation for step title

--- src/components/Spontanei/steps/Riepilogo.tsx
+++ src/components/Spontanei/steps/Riepilogo.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Autocomplete, Card, Stack, TextField, Typography } from '@mui/material';
+import { Card, Stack, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { PaymentNoticeDetailsDTO } from '../../../../generated/apiClient';
-
+import { PaymentNotice } from 'components/PaymentNotice';
+import { PaymentNoticeEnum } from 'models/PaymentNotice';
+import utils from 'utils';
 export type Servizio = 'Iscrizione Scuola Materna' | 'Rinnovo Licenza Caccia' | 'Bollo Auto';
 
 interface SelezionaServzioProps {
@@ -11,12 +13,28 @@ interface SelezionaServzioProps {
 
 const Riepilogo = (props: SelezionaServzioProps) => {
   const { t } = useTranslation();
+  const { paFullName = '', paTaxCode = '', iupd = '', paymentOptions = [] } = props.spontaneo;
   return (
     <Card variant="outlined">
       <Stack spacing={2} padding={4}>
         <Typography variant="h6">{t('spontanei.form.steps.step4.title')}</Typography>
         <Typography>{t('spontanei.form.steps.step4.description')}</Typography>
-        {JSON.stringify(props.spontaneo)}
+        <PaymentNotice.Detail
+          paymentNotice={{
+            type: PaymentNoticeEnum.SINGLE,
+            paTaxCode,
+            paFullName,
+            iupd,
+            paymentOptions: {
+              amount: utils.converters.toEuro(paymentOptions[0].amount || 0),
+              amountValue: paymentOptions[0].amount || 0,
+              nav: paymentOptions[0].nav || '',
+              iuv: paymentOptions[0].iuv || '',
+              description: paymentOptions[0].description || '',
+              dueDate: utils.datetools.formatDate(paymentOptions[0].dueDate)
+            }
+          }}
+        />
       </Stack>
     </Card>
   );
