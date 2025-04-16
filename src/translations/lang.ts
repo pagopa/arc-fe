f71ffa7809ca8c711cdf6b3eec6a991c5aa973b8
Strato Ivan Di Luccio <34749257+stratoivandiluccio@users.noreply.github.com>
2025-04-16 14:16:24 +0200

    feat: [P4PU-964] french translations (#280)

--- src/translations/lang.ts
+++ src/translations/lang.ts
@@ -1,5 +1,6 @@
 import translationIT from './it/translations.json';
 import translationEN from './en/translations.json';
+import translationFR from './fr/translations.json';
 
 const lang = {
   it: {
@@ -11,6 +12,11 @@ const lang = {
     label: 'English',
     lang: 'en-EN',
     translation: translationEN
+  },
+  fr: {
+    label: 'French',
+    lang: 'fr-FR',
+    translation: translationFR
   }
 };
 /* Are we sure we need a sorting function? The order wouldn't be the one commonly intended by us (which is usually: IT, EN, FR...)*/
